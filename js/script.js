44882ce8849e03837b89a648a2b522d8c304600f
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-30 11:15:48 -0700

    done
    co-authored-by: Erik Zambrano <molagg92@gmail.com>
    dquote> co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@ function ccValidate(number) {
     let numberArray = number.toString().split("").map(function(digit){return parseInt(digit)});
     
     const newArray = [];
-    for(let i = 1; i <= numberArray.length; i += 2 ) {
-        newArray.push(numberArray[i] * 2);
-        console.log(newArray);
-    }
+    // for(let i = 1; i <= numberArray.length; i += 2 ) {
+    //     numberArray.splice(1, i, ([i]*2));
+    //     console.log(numberArray);
+    // }
 
+    for (let i = 0; i <= numberArray.length; i ++){
+        if (i % 2 !== 0) {
+            newArray.push(numberArray[i] * 2);
+        }else {
+        	newArray.push(numberArray[i]);	
+    } 
+	}
+	console.log(newArray);
 }
