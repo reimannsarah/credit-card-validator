653215eb387e400abf3238b06df701ae4f44cf1b
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-30 10:31:26 -0700

    pass test 3
    co-authored-by: Erik Zambrano <molagg92@gmail.com>
    dquote> co-authored-by: Raed Alkhanbashi <raed.alkhanbashi@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-function ccValidate(number){
-    let stringNumber = number.toString().split("").map(function(digit){return parseInt(digit)});
-    return stringNumber;
+function ccValidate(number) {
+    let numberArray = number.toString().split("").map(function(digit){return parseInt(digit)});
+    
+    const newArray = [];
+    for(let i = 1; i <= numberArray.length; i += 2 ) {
+        newArray.push(numberArray[i] * 2);
+        console.log(newArray);
+    }
 
 }
