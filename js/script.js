f2d81c34b301d05bdf4c09477c9c4e4f70e5a63d
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-30 21:39:21 -0700

    WIP: still working on conditionals, discovered there may be a problem with the array lengths

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ function ccValidate(number) {
     const newArray = [];
     let sum = 0;
 
-    let firstTwoNumbers = numberArray.slice(0,2);
-
-    for (let i = 0; i < numberArray.length; i ++){
+    for (let i = 0; i <= numberArray.length-1; i ++){
         if (i % 2 !== 0) {
             let doubleI = numberArray[i] * 2;
             if (doubleI>=10){
@@ -14,28 +12,32 @@ function ccValidate(number) {
                 let integer2 = arrayDoubleI[1];
                 let addDoubleI = integer1 + integer2;
                 newArray.push(addDoubleI);
+                console.log(newArray);
             }else {
                 newArray.push(numberArray[i] * 2);
                }
         }else {
         newArray.push(numberArray[i]);
         };	
-        sum += newArray[i]     
-        return sum;   
+        sum += newArray[i] 
+        console.log(sum);      
     }  
-}
-
     
-    // let sumArray = sum.toString().split("").map(Number);
-    // let lastNumber = sumArray.pop();
-    // if (lastNumber === 0 && firstNumber === 4 || firstNumber === 5 || firstNumber === 6 ){
-    //     return "This card number is valid.";
-    // }else if (lastNumber === 0 && firstTwoNumbers === 34 || firstTwoNumbers === 37){
-    //     return "This card number is valid.";
-    // }else {
-    //     return "This card number is not valid.";
-    // };
-	// }
+    let sumArray = sum.toString().split("").map(Number);
+    let firstTwoNumbers = numberArray.slice(0, 2);
+    let firstNumber = firstTwoNumbers[0];
+    let secondNumber = firstTwoNumbers[1];
+    let lastNumber = sumArray.pop();
+    if (firstNumber === 4 || firstNumber === 5 || firstNumber === 6 && lastNumber === 0){
+        return "This card number is valid.";
+    }else if (firstNumber === 3 && secondNumber === 4 && lastNumber === 0){
+        return "This card number is valid.";
+    }else if (firstNumber === 3 && secondNumber === 7 && lastNumber ===0){
+        return "This card number is valid.";
+    }else {
+        return "This card number is not valid.";
+    };
+	}
 	
 
 
