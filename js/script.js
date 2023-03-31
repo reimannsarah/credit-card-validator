bb4b749206c55c76d0daa2f3ec2e394bde6df6d0
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-30 20:29:25 -0700

    WIP:working on if/else statements to evaluate the first one or two digits of the credit card number

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ function ccValidate(number) {
     const newArray = [];
     let sum = 0;
 
+    let firstTwoNumbers = numberArray.slice(0,2);
+
     for (let i = 0; i < numberArray.length; i ++){
         if (i % 2 !== 0) {
             let doubleI = numberArray[i] * 2;
@@ -18,16 +20,22 @@ function ccValidate(number) {
         }else {
         newArray.push(numberArray[i]);
         };	
-        sum += newArray[i]        
+        sum += newArray[i]     
+        return sum;   
     }  
-    let sumArray = sum.toString().split("").map(Number);
-    let lastNumber = sumArray.pop();
-    if (lastNumber === 0){
-        return "This card number is valid.";
-    } else {
-        return "This card number is not valid.";
-    };
-	}
+}
+
+    
+    // let sumArray = sum.toString().split("").map(Number);
+    // let lastNumber = sumArray.pop();
+    // if (lastNumber === 0 && firstNumber === 4 || firstNumber === 5 || firstNumber === 6 ){
+    //     return "This card number is valid.";
+    // }else if (lastNumber === 0 && firstTwoNumbers === 34 || firstTwoNumbers === 37){
+    //     return "This card number is valid.";
+    // }else {
+    //     return "This card number is not valid.";
+    // };
+	// }
 	
 
 
