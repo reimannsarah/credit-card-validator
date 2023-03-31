76d5f1205bf47b9b9277a4afbee9d31473be3ff2
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-30 18:09:49 -0700

    add if else statement to sum the two separate integers of a double-digit number within the array

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 function ccValidate(number) {
-    let numberArray = number.toString().split("").map(function(digit){return parseInt(digit)});
+    let numberArray = number.toString().split("").map(Number);
     
     const newArray = [];
-    // for(let i = 1; i <= numberArray.length; i += 2 ) {
-    //     numberArray.splice(1, i, ([i]*2));
-    //     console.log(numberArray);
-    // }
+ 
 
     for (let i = 0; i <= numberArray.length; i ++){
         if (i % 2 !== 0) {
-            newArray.push(numberArray[i] * 2);
+            let doubleI = numberArray[i] * 2;
+            if (doubleI>=10){
+                let arrayDoubleI = doubleI.toString().split("").map(Number);
+                let integer1 = arrayDoubleI[0];
+                let integer2 = arrayDoubleI[1];
+                let addDoubleI = integer1 + integer2;
+                newArray.push(addDoubleI);
+            }else {
+                    newArray.push(numberArray[i] * 2);
+               }
         }else {
-        	newArray.push(numberArray[i]);	
+        newArray.push(numberArray[i]);
+    };	
     } 
+    return newArray;
 	}
-	console.log(newArray);
-}
+	
+
+
+
